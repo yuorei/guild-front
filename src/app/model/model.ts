897ed9c7ecf36eaf9cc99ff4802ee3d5c8f8f2ee
yuorei <108039575+yuorei@users.noreply.github.com>
2023-11-15 10:28:17 +0900

    Board

---
+++ src/app/model/model.ts
@@ -0,0 +1,14 @@
+export type Board = {
+    id: string;
+    user_id: string;
+    title: string;
+    description: string;
+    reward: string;
+    endDate: string;
+    lebel: string;
+    max: number;
+    min: number;
+    imageURL: string;
+    createdAt: string;
+    updatedAt: string;
+};
